f6b764aa80a76a3436234e4f274bf1c9bf523a76
Sigaretif <215838@edu.p.lodz.pl>
2022-01-21 17:31:50 +0100

    1.4 accesory 1 - zmiana widocznosci atrybutow klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,13 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int NUMBERS = 12;
-    public static final int INT = -1;
-    public static final int NEGATIVE_TOTAL_NUMBER = -1;
-    public static final int FULL = 11;
+    private static final int NUMBERS = 12;
+    private static final int INT = -1;
+    private static final int NEGATIVE_TOTAL_NUMBER = -1;
+    private static final int FULL = 11;
     private final int[] numbers = new int[NUMBERS];
 
-    public int total = NEGATIVE_TOTAL_NUMBER;
+    private int total = NEGATIVE_TOTAL_NUMBER;
 
     public void countIn(int in) {
         if (!isFull())
